835fea60599e90e4a6f8d6f574dfcdb925bc5791
CoLTE Community LTE <colte@zbox-epc-4k.cs.washington.edu>
2018-08-08 00:27:16 +0000

    check billing website status by using curl

--- lte_extras/service_monitor/routes/index.js
+++ lte_extras/service_monitor/routes/index.js
@@ -4,9 +4,12 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const { execSync } = require('child_process');
-  let stdout = execSync('pwd');
-  console.log(stdout.toString());
-  res.render('index', { title: 'CoLTE System Status' });
+  let stdout = execSync('curl -vsL -o /dev/null http://localhost:3002 2>&1 | grep "200 OK"');
+  let pwd = (stdout.toString().trim() == "< HTTP/1.1 200 OK") ? 1 : 0;
+  res.render('index', { 
+	  title: 'CoLTE System Status',
+	  pwd: pwd
+  });
 });
 
 module.exports = router;
